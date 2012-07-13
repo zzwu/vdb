f3a4c8cde7ceef289c08649faf4efce22b61206e
zzwu <zhenzhouwu@yahoo.cn>
2012-07-13 16:10:18 +0800

    client detail paramaters

--- src/vdb/client.clj
+++ src/vdb/client.clj
@@ -3,22 +3,41 @@
            [voldemort.versioning Versioned])
   (:refer-clojure :exclude [get with-open]))
 
+;;{:url "tcp://localhost:6666" :name "test" :client-config {"bootstrap_urls" "tcp://localhost:6666""socket_timeout_ms" "5000"}}
+;;{:url "tcp://localhost:6666" :name "test" :client-config nil}
 (def test-spec
   {:url "tcp://localhost:6666" :name "test"})
 
 (def ^:dynamic *client*)
 
 (defmacro with-open
-  [client-sepc & body]
-  `(binding [*client* (client (factory (:url ~client-sepc)) (:name ~client-sepc))]
+  [sepc & body]
+  `(binding [*client* (client (factory ~sepc) (:name ~sepc))]
      ~@body))
 
-(def factory
-  (memoize
-    (fn [url]
-    (let [config (ClientConfig.)
+(defmulti make-factory (fn [spec] (:client-config spec)))
+
+(defmethod make-factory nil
+  [spec]
+  (let [config (ClientConfig.)
+        url (:url spec)
         factory (SocketStoreClientFactory. (.setBootstrapUrls config (into-array String (vector url))))]
-      factory))))
+      factory))
+
+(defn props-from-map
+  [config]
+  (let [c (into {} (for [k (keys config)] (if (clojure.core/get config k) [k (clojure.core/get config k)])))]
+    (doto (java.util.Properties.)
+      (.putAll c))))
+
+(defmethod make-factory :default
+  [spec]
+  (let [config (ClientConfig. (props-from-map (:client-config spec)))
+        factory (SocketStoreClientFactory. config)]
+      factory))
+
+(def factory
+  (memoize make-factory))
 
 (defn close-factory
   [url]
